e30cb92eecff8f0a900a25a8ea1daf6d3358295b
Juanelboi <juangarciaboissier@gmail.com>
2025-05-17 18:42:48 +0100

    fix

--- src/app/components/secondary-header/secondary-header.component.ts
+++ src/app/components/secondary-header/secondary-header.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { FirestoreService } from '../../services/firestore/firestore.service';
+import { GameSearchService } from '../../services/game-search/game-search.service';
+import { Router } from '@angular/router';
+import { UserData } from '../../models/user';
+import { User } from 'firebase/auth';
+import {CommonModule, NgIf} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-secondary-header',
   imports: [
-    RouterLink,
     FormsModule,
-    IonicModule
+    IonicModule,
+    NgIf,
   ],
   templateUrl: './secondary-header.component.html',
   styleUrl: './secondary-header.component.scss'
 })
 export class SecondaryHeaderComponent {
-  selectedLanguage: string = "en";
+  searchQuery: string = '';
+  selectedLanguage: string = 'en';
+  user: User | null = null;
+  userData: UserData | null = null;
+
+  constructor(
+    private router: Router,
+    private gameSearchService: GameSearchService,
+    private authService: AuthService,
+    private firestoreService: FirestoreService
+  ) {}
+
+  ngOnInit() {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+      if (this.user) {
+        this.firestoreService.loadUser(this.user.uid).subscribe(userData => this.userData = userData);
+      }
+    });
+  }
+
+
+  protected readonly encodeURI = encodeURI;
 }
